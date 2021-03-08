c26ce671bd68a7689ff4325b5f8cd31da4ae34bc
Marcus-Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-08 17:24:20 -0500

    Add files via upload

---
+++ Monday/Group Project.js
@@ -0,0 +1,31 @@
+function newElement(){
+    var li = document.createElement('li');
+    var inputValue = document.getElementById("myInput").value;
+    li.appendChild(document.createTextNode(inputValue));
+    if (inputValue === '') {
+        alert("You must write something!");
+      } else {
+        document.getElementById("list").appendChild(li);
+      }
+    
+    // creating check boxes
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.value = 1;
+    li.appendChild(checkbox);
+    checkbox.addEventListener('change', function(e){
+        if(this.checked){
+        e.currentTarget.parentNode.style.textDecoration = "line-through";
+    }
+        else {e.currentTarget.parentNode.style.textDecoration ="none"};
+});
+    document.getElementById("myInput").value = "";
+    // creating button to remove shit
+    var rmvButton = document.createElement("button");
+    rmvButton.innerHTML = "Remove";
+    li.appendChild(rmvButton);
+    rmvButton.className = "removebutton";
+    rmvButton.addEventListener('click', function(e) {
+        e.currentTarget.parentNode.remove();
+    });
+}
